1df2a2753ed058f4b0787d4ca4a0336dfba0c5e8
St0n3k <kamillokoks@gmail.com>
2023-03-25 17:08:02 +0100

    Zdefiniowano automatyczną konfigurację pul połączeń dla jednostek składowania ssbd05adminPU oraz ssbd05mokPU. Zdefiniowano te jednostki składowania w pliku persistence.xml. SSBD202305-23

---
+++ src/main/java/pl/lodz/p/it/ssbd2023/config/JDBCConfig.java
@@ -0,0 +1,38 @@
+package pl.lodz.p.it.ssbd2023.config;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.sql.Connection;
+
+@DataSourceDefinition(
+        name = "java:app/jdbc/ssbd05admin",
+        className = "org.postgresql.ds.PGSimpleDataSource",
+        user = "ssbd05admin",
+        password = "admin",
+        serverName = "db",
+        portNumber = 5432,
+        databaseName = "ebok",
+        initialPoolSize = 1,
+        minPoolSize = 0,
+        maxPoolSize = 1,
+        maxIdleTime = 10)
+
+@DataSourceDefinition(
+        name = "java:app/jdbc/ssbd05mok",
+        className = "org.postgresql.ds.PGSimpleDataSource",
+        user = "ssbd05mok",
+        password = "mok",
+        serverName = "db",
+        portNumber = 5432,
+        databaseName = "ebok",
+        transactional = true,
+        isolationLevel = Connection.TRANSACTION_READ_COMMITTED)
+
+@Stateless
+public class JDBCConfig {
+    @PersistenceContext(unitName = "ssbd05adminPU")
+    private EntityManager em;
+}
