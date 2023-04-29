84ea97cf1539741da2a34d8de19a0d9e9c69270c
AverageCoffeeEnjoyer <bartlomiej.czajkowski20@gmail.com>
2023-04-29 21:17:00 +0200

    Reverting unnecessary changes, small changes SSBD202305-125

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd05/mok/cdi/endpoint/dto/request/ChangeActiveStatusDto.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd05/mok/cdi/endpoint/dto/request/ChangeActiveStatusDto.java
@@ -1,6 +1,6 @@
 package pl.lodz.p.it.ssbd2023.ssbd05.mok.cdi.endpoint.dto.request;
 
-import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,8 +11,8 @@ import lombok.NoArgsConstructor;
 @Data
 public class ChangeActiveStatusDto {
 
-    @Id
-    private String login;
+    @NotBlank
+    private String id;
 
     @NotNull
     private Boolean active;
