c65a8286332627ed7d0c22c57ba50416fa5660c7
tomczykp <tomczykp987@gmail.com>
2023-05-02 16:47:01 +0200

    Checkstyle fix, SSBD202305-134

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd05/mok/cdi/endpoint/dto/request/EditOwnPersonalDataDto.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd05/mok/cdi/endpoint/dto/request/EditOwnPersonalDataDto.java
@@ -1,7 +1,7 @@
 package pl.lodz.p.it.ssbd2023.ssbd05.mok.cdi.endpoint.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
