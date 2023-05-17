1b0eb4f8e2703a9547e00bb91139a6fe231b2703
tomczykp <tomczykp987@gmail.com>
2023-05-17 14:14:12 +0200

    Dodanie serwera pośredniczącego do infrastruktury testowej wraz z koniecznymi plikami, SSBD202305-220

--- backend/src/test/java/TestContainersSetup.java
+++ backend/src/test/java/TestContainersSetup.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
+import org.testcontainers.containers.NginxContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -17,6 +18,17 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+/*
+    W celu uruchomienia testów należy dodać pliki certyfikatu, z podowdów bezpieczeństwa nie zostały one umieszczone w
+    repozytorium. Pliki potrzebne do uruchomienia to:
+        - fullchain.pem
+        - privkey.pem
+        Należy je umięcić w katalogu: /backend/src/resources/data/gw/letsencrypt/live/team-5.proj-sum.it.p.lodz.pl/
+    Oraz plik:
+        - ssl-dhparams.pem
+        Który należy umieścić w katalogu: /backend/src/resources/data/gw/letsencrypt/
+ */
+
 @Testcontainers
 public class TestContainersSetup {
     protected static RequestSpecification ownerSpec;
@@ -24,6 +36,7 @@ public class TestContainersSetup {
     protected static RequestSpecification adminSpec;
     private static GenericContainer postgresContainer;
     private static GenericContainer payaraContainer;
+    private static GenericContainer nginxContainer;
 
     @BeforeAll
     static void setup() throws IOException {
@@ -53,10 +66,33 @@ public class TestContainersSetup {
             .withReuse(true);
 
 
+        MountableFile nginxConfFile = MountableFile.forHostPath(
+            Paths.get(new File("src/test/resources/data/gw/nginx/nginx.conf").getCanonicalPath()).toAbsolutePath(),
+            0777);
+
+        MountableFile mimeFile = MountableFile.forHostPath(
+            Paths.get(new File("src/test/resources/data/gw/nginx/mime.types").getCanonicalPath()).toAbsolutePath(),
+            0777);
+        nginxContainer = new NginxContainer<>("nginx:latest")
+            .withExposedPorts(443)
+            .dependsOn(payaraContainer)
+            .withNetwork(network)
+            .withNetworkAliases("gw")
+            .withCopyFileToContainer(nginxConfFile, "/etc/nginx/nginx.conf")
+            .withFileSystemBind(
+                Paths.get(new File("src/test/resources/data/gw/letsencrypt").getCanonicalPath()).toAbsolutePath()
+                    .toString(),
+                "/etc/letsencrypt")
+            .withCopyFileToContainer(mimeFile, "/etc/nginx/mime.types");
+
         postgresContainer.start();
         payaraContainer.start();
+        nginxContainer.start();
 
-        RestAssured.baseURI = "http://localhost:" + payaraContainer.getMappedPort(8080) + "/eBok";
+        RestAssured.baseURI = "https://localhost"
+            + ":" + nginxContainer.getMappedPort(443)
+            + "/api";
+        RestAssured.useRelaxedHTTPSValidation();
 
         generateOwnerSpec();
         generateManagerSpec();
@@ -65,6 +101,7 @@ public class TestContainersSetup {
 
     @AfterAll
     static void cleanup() {
+        nginxContainer.stop();
         payaraContainer.stop();
         postgresContainer.stop();
     }
