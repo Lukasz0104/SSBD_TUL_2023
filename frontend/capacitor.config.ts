6fa6890767f8de24b5c3d84a4bf619dd6235746e
Msocha19 <msocha192000@gmail.com>
2023-06-15 23:35:13 +0200

    Zmiana nazwy generowanej aplikacji mobilnej na "eBok" SSBD202305-339

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'pl.lodz.p.it.ssbd2023.ssbd05',
-  appName: 'frontend',
-  webDir: 'dist/frontend',
-  server: {
-    androidScheme: 'https'
-  }
+    appId: 'pl.lodz.p.it.ssbd2023.ssbd05',
+    appName: 'eBok',
+    webDir: 'dist/frontend',
+    server: {
+        androidScheme: 'https'
+    }
 };
 
 export default config;
