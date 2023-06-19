609b9b2c4e4a6ac222b006a3f2ccab6ed5fa7716
St0n3k <kamillokoks@gmail.com>
2023-06-19 21:09:47 +0200

    Uwzględniono dwa nowe przypadki krytyczne w obsłudze błędów podczas usuwania przyszłej stawki. Dodano gwiazdki przy wymaganych polach formularza podczas edycji danych własnego konta oraz dodawania nowej stawki. https://atlas.it.p.lodz.pl/jira/browse/SSBD202305-376

--- backend/src/main/java/pl/lodz/p/it/ssbd2023/ssbd05/interceptors/RateFacadeExceptionsInterceptor.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2023/ssbd05/interceptors/RateFacadeExceptionsInterceptor.java
@@ -7,6 +7,7 @@ import jakarta.persistence.PersistenceException;
 import org.eclipse.persistence.exceptions.DatabaseException;
 import org.postgresql.util.PSQLException;
 import pl.lodz.p.it.ssbd2023.ssbd05.exceptions.AppDatabaseException;
+import pl.lodz.p.it.ssbd2023.ssbd05.exceptions.conflict.RateAlreadyEffectiveException;
 import pl.lodz.p.it.ssbd2023.ssbd05.exceptions.conflict.RateNotUniqueException;
 
 public class RateFacadeExceptionsInterceptor {
@@ -22,6 +23,9 @@ public class RateFacadeExceptionsInterceptor {
                 String exceptionMessage = pe.getMessage();
                 if (exceptionMessage.contains("unq_rate_0")) {
                     throw new RateNotUniqueException();
+                } else if (exceptionMessage.contains("fk_forecast_rate_id") ||
+                    exceptionMessage.contains("fk_place_rate_rate_id")) {
+                    throw new RateAlreadyEffectiveException();
                 }
                 pe = pe.getCause();
             } while (pe != null);
